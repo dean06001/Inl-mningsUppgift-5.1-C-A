9674929d3c6da90c2c6222dac5252c7518a8b830
dean06001 <dean06001@E-PW0234KC>
2023-11-28 19:45:13 +0100

    Add project files.

---
+++ InlämningsUppgift 5.1 C-A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace InlämningsUppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programmet skriver ut ett par meningar.");
+            string[] meningar = { "Jag tycker om att träna.", "Gillar också mat.", "Detta är sista meningen." };
+            for (int i = 0; i < meningar.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(meningar[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
